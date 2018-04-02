773051873520116fc4a0c2a372853a2f1be705f8
FelixWaweru <wawerufelix60@gmail.com>
2018-04-02 19:38:00 +0300

    Batch script upload

---
+++ Batch Script/Becks.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Running Beck's Depression Inventory.
+echo.
+echo Hey Felix. Ready for your weekly quiz?
+echo.
+type BecksHeader.txt
+echo.
+
+cd C:\Users\USER PC\Documents\GitHub\Becks_Depression_Inventory
+Questionnaire.py
+PAUSE
